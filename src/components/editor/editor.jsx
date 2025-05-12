557c4dfc5b3c21efb03c18509745b9dc1f9ab90b
Bamsemats <mats.f.ronnqvist@gmail.com>
2025-05-12 12:58:08 +0200

    Setup

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -5,8 +5,8 @@ import { Color } from '@tiptap/extension-color';
 import ListItem from '@tiptap/extension-list-item';
 import TextStyle from '@tiptap/extension-text-style';
 import StarterKit from '@tiptap/starter-kit';
-import React, { useEffect, useRef } from 'react';
-import {useState} from 'react';
+import Image from '@tiptap/extension-image';
+import { useEffect, useRef, useState } from 'react';
 import { BiBold, BiCodeAlt, BiRedo, BiStrikethrough, BiUndo } from "react-icons/bi";
 import { BiItalic } from "react-icons/bi";
 import { AiOutlineUnorderedList } from "react-icons/ai";
@@ -16,12 +16,26 @@ import DOMPurify from 'dompurify';
 import { BsBlockquoteRight } from 'react-icons/bs';
 import { GoHorizontalRule } from 'react-icons/go';
 import { VscNewline } from 'react-icons/vsc';
-import { LuChevronDown, LuHeading1, LuHeading2, LuHeading3, LuHeading4, LuHeading5, LuHeading6 } from 'react-icons/lu';
+import { LuChevronDown, LuHeading1, LuHeading2, LuHeading3, LuHeading4, LuHeading5, LuHeading6, LuImage, LuPalette, LuSquare } from 'react-icons/lu';
 
 // define your extension array
-const extensions = [StarterKit]
+const extensions = [
+  Image,
+  Color.configure({ types: [TextStyle.name, ListItem.name] }),
+  TextStyle.configure({ types: [ListItem.name] }),
+  StarterKit.configure({
+    bulletList: {
+      keepMarks: true,
+      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+    },
+    orderedList: {
+      keepMarks: true,
+      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+    },
+  }),
+]
 
-const content = '<p>Hello World!</p>'
+const content = '<p>Hey! Write something cool..!</p>'
 
 const Tiptap = () => {
 
@@ -47,16 +61,59 @@ const Tiptap = () => {
   }
 
   const MenuBar = () => {
-    const { editor } = useCurrentEditor()
-    
+    const { editor } = useCurrentEditor();
     const [currentHeading, setCurrentHeading] = useState('');
     const [headingDropDownShown, setHeadingDropDownShown] = useState(false);
+    const [colorDropDownShown, setColorDropDownShown] = useState(false);
     const headingDropDownRef = useRef(null);
     const headingDropDownButtonRef = useRef(null);
+    const colorDropDownRef = useRef(null);
+    const colorDropDownButtonRef = useRef(null);
 
     const activeHeading = editor.isActive('heading', {level: 2}) ? 'H2' : editor.isActive('heading', {level: 3}) ? 'H3' : editor.isActive('heading', {level: 4}) ? 'H4' : editor.isActive('heading', {level: 5}) ? 'H5' : editor.isActive('heading', {level: 6}) ? 'H6' : 'H1';
 
-    console.log(document.querySelector('.heading-dropdown-menu'));
+    const colorList = [
+      "#000000", // Pure Black
+      "#111111", // Jet Black
+      "#212121", // Charcoal
+      "#2E2E2E", // Dark Slate Gray
+      "#4F4F4F", // Dim Gray
+      "#6E6E6E", // Gray
+      "#708090", // Slate Gray
+      "#A0A0A0", // Light Gray
+      "#DCDCDC", // Gainsboro
+      "#F5F5F5", // White Smoke
+      "#FFFFFF", // Pure White
+      "#0d6efd", // Blue (Link)
+      "#6f42c1", // Blue (Visited)
+      "#1E90FF", // Dodger Blue
+      "#4169E1", // Royal Blue
+      "#DC143C", // Crimson
+      "#B22222", // Firebrick
+      "#228B22", // Forest Green
+      "#DAA520", // Goldenrod
+      "#FF1493"  // Deep Pink
+    ]
+
+    const ColorElement = () => {
+      colorList.map(color => {
+        return (
+          <button
+            onClick={() => editor.chain().focus().setColor(color).run()}
+            className={editor.isActive('textStyle', { color: color }) ? 'is-active' : ''}
+          >
+            <LuSquare style={{color: color, fill: color}}/>
+          </button>
+        )
+      })
+    }
+
+    // <button
+    //             onClick={() => editor.chain().focus().setColor('#958DF1').run()}
+    //             className={editor.isActive('textStyle', { color: '#958DF1' }) ? 'is-active' : ''}
+    //           >
+    //             <LuSquare style={{color: '#958DF1', fill: '#958DF1'}}/>
+    //           </button>
 
     useEffect(() => {
       setCurrentHeading((prev) => ({
@@ -83,7 +140,7 @@ const Tiptap = () => {
     }
 
     useEffect(() => {
-      function handleClickOutside(event) {
+      function handleClickOutsideHeading(event) {
         if (
           headingDropDownRef.current &&
           !headingDropDownRef.current.contains(event.target) &&
@@ -95,12 +152,62 @@ const Tiptap = () => {
         }
       }
   
-      document.addEventListener('mousedown', handleClickOutside);
+      document.addEventListener('mousedown', handleClickOutsideHeading);
   
       return () => {
-        document.removeEventListener('mousedown', handleClickOutside);
+        document.removeEventListener('mousedown', handleClickOutsideHeading);
       };
     }, []);
+
+    function handleClickColorDropdown(event) {
+      event.stopPropagation(); // Prevent the click from propagating to the document
+      setColorDropDownShown((prevState) => {
+        const dropDownMenu = document.querySelector('.color-dropdown-menu');
+        if (!prevState) {
+          dropDownMenu.classList.add('color-show'); // Open the menu
+        } else {
+          dropDownMenu.classList.remove('color-show'); // Close the menu
+        }
+        return !prevState; // Toggle the state
+      });
+    }
+
+    useEffect(() => {
+      function handleClickOutsidecolor(event) {
+        if (
+          colorDropDownRef.current &&
+          !colorDropDownRef.current.contains(event.target) &&
+          colorDropDownButtonRef.current &&
+          !colorDropDownButtonRef.current.contains(event.target)
+        ) {
+          colorDropDownRef.current.classList.remove('color-show');
+          setColorDropDownShown(false);
+        }
+      }
+  
+      document.addEventListener('mousedown', handleClickOutsidecolor);
+  
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutsidecolor);
+      };
+    }, []);
+
+    const getCurrentTextColor = () => {
+      const attributes = editor.getAttributes('textStyle');
+      return attributes.color || 'default';
+    }
+
+    const currentColor = getCurrentTextColor();
+
+    const addImage = () => {
+      const url = window.prompt('URL')
+
+      if (url) {
+        editor.chain().focus().setImage({ src: url }).run()
+      }
+    }
+
+    console.log(currentColor)
 
     if (!editor) {
       return null
@@ -121,6 +228,9 @@ const Tiptap = () => {
             className={editor.isActive('bold') ? 'is-active' : ''}
           >
             <BiBold />
+          </button>
+          <button onClick={addImage}>
+            <LuImage />
           </button>
           <button
             onClick={() => editor.chain().focus().toggleItalic().run()}
@@ -189,37 +299,37 @@ const Tiptap = () => {
               onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
               className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
               >
-                H1
+                <LuHeading1 />
               </button>
               <button
                 onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
                 className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
               >
-                H2
+                <LuHeading2 />
               </button>
               <button
                 onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
                 className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
               >
-                H3
+                <LuHeading3 />
               </button>
               <button
                 onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
                 className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}
               >
-                H4
+                <LuHeading4 />
               </button>
               <button
                 onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
                 className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}
               >
-                H5
+                <LuHeading5 />
               </button>
               <button
                 onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
                 className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}
               >
-                H6
+                <LuHeading6 />
               </button>
             </div>
           </div>
@@ -278,12 +388,29 @@ const Tiptap = () => {
           >
             <BiRedo />
           </button>
-          <button
-            onClick={() => editor.chain().focus().setColor('#958DF1').run()}
-            className={editor.isActive('textStyle', { color: '#958DF1' }) ? 'is-active' : ''}
-          >
-            Purple {`(Not working)`}
-          </button>
+          <div className='color-dropdown-menu-div'>
+            <button 
+              className='color-dropdown-menu-button'
+              onClick={handleClickColorDropdown}
+            >
+              <LuPalette style={{color: currentColor}}/>
+            </button>
+            <div className='color-dropdown-menu'>
+              {
+                colorList.map(color => {
+                  return (
+                    <button
+                      onClick={() => editor.chain().focus().setColor(color).run()}
+                      className={editor.isActive('textStyle', { color: color }) ? 'is-active' : ''}
+                    >
+                      <LuSquare style={{color: color, fill: color}}/>
+                    </button>
+                  )
+                })
+              }
+            </div>
+          </div>
+          
         </div>
       </div>
     )
