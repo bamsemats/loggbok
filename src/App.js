b6526694b503a795fb5060901d0bda695c911aae
Bamsemats <mats.f.ronnqvist@gmail.com>
2025-05-16 10:47:17 +0200

    Update heading and bug in dropdown menus

--- src/App.js
+++ src/App.js
@@ -6,17 +6,26 @@ function App() {
   return (
     <div className="App">
       <h1 className="main-page-heading">
-        <span style={{ "--sibling-index": 1 }}>T</span>
-        <span style={{ "--sibling-index": 2 }}>h</span>
-        <span style={{ "--sibling-index": 3 }}>i</span>
-        <span style={{ "--sibling-index": 4 }}>{`s `}</span>
-        <span style={{ "--sibling-index": 5 }}>i</span>
-        <span style={{ "--sibling-index": 6 }}>{`s `}</span>
-        <span style={{ "--sibling-index": 7 }}>a</span>
-        <span style={{ "--sibling-index": 8 }}>{`n `}</span>
-        <span style={{ "--sibling-index": 9 }}>A</span>
-        <span style={{ "--sibling-index": 10 }}>p</span>
-        <span style={{ "--sibling-index": 11 }}>p</span>
+        <h1 style={{ "--sibling-index": 1 }}>T</h1>
+        <h1 style={{ "--sibling-index": 2 }}>H</h1>
+        <h1 style={{ "--sibling-index": 3 }}>I</h1>
+        <h1
+          className="end-of-word-letter"
+          style={{ "--sibling-index": 4 }}
+        >{`S `}</h1>
+        <h1 style={{ "--sibling-index": 5 }}>I</h1>
+        <h1
+          className="end-of-word-letter"
+          style={{ "--sibling-index": 6 }}
+        >{`S `}</h1>
+        <h1 style={{ "--sibling-index": 7 }}>A</h1>
+        <h1
+          className="end-of-word-letter"
+          style={{ "--sibling-index": 8 }}
+        >{`N `}</h1>
+        <h1 style={{ "--sibling-index": 9 }}>A</h1>
+        <h1 style={{ "--sibling-index": 10 }}>P</h1>
+        <h1 style={{ "--sibling-index": 11 }}>P</h1>
       </h1>
       <Tiptap />
     </div>
